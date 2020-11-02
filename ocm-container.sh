b7c805f69b2176bf605172fa1961e88648df42fd
Kirk Bater <kirk.bater@gmail.com>
2020-11-02 15:39:18 -0500

    Adds browserless login

--- ocm-container.sh
+++ ocm-container.sh
@@ -16,6 +16,7 @@ fi
 
 source ${OCM_CONTAINER_CONFIGFILE}
 
+### SSH Agent Mounting
 operating_system=`uname`
 
 SSH_AGENT_MOUNT="-v ${SSH_AUTH_SOCK}:/tmp/ssh.sock:ro"
@@ -25,14 +26,31 @@ then
   SSH_AGENT_MOUNT="--mount type=bind,src=/run/host-services/ssh-auth.sock,target=/tmp/ssh.sock,readonly"
 fi
 
+### Kerberos Ticket Mounting
+OCM_CONTAINER_KRB5CC_FILE=${KRB5CCNAME:-/tmp/krb5cc_$UID}
+if [ -f $OCM_CONTAINER_KRB5CC_FILE ]
+then
+  KRB5CCFILEMOUNT="-v ${OCM_CONTAINER_KRB5CC_FILE}:/tmp/krb5cc:ro"
+fi
+
+### Automatic Login Detection
+if [ -n "$1" ]
+then
+  INITIAL_CLUSTER_LOGIN="-e INITIAL_CLUSTER_LOGIN=$1"
+fi
+
 ### start container
 ${CONTAINER_SUBSYS} run -it --rm --privileged \
 -e "OCM_URL=${OCM_URL}" \
 -e "SSH_AUTH_SOCK=/tmp/ssh.sock" \
+-e "KRB5CCNAME=/tmp/krb5cc" \
+-e "OFFLINE_ACCESS_TOKEN" \
+${INITIAL_CLUSTER_LOGIN} \
 -v ${CONFIG_DIR}:/root/.config/ocm-container:ro \
-${SSH_AGENT_MOUNT} \
 -v ${HOME}/.ssh:/root/.ssh:ro \
 -v ${HOME}/.aws/credentials:/root/.aws/credentials:ro \
 -v ${HOME}/.aws/config:/root/.aws/config:ro \
+${SSH_AGENT_MOUNT} \
+${KRB5CCFILEMOUNT} \
 ${OCM_CONTAINER_LAUNCH_OPTS} \
-ocm-container /bin/bash 
+ocm-container
