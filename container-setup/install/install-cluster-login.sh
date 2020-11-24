8d597c99d39dfae2b7762a54cf51bd615ea1f8e8
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-11-24 12:51:18 +0200

    feat(srelogin): make srelogin upstreamable

--- container-setup/install/install-cluster-login.sh
+++ container-setup/install/install-cluster-login.sh
@@ -2,3 +2,14 @@
 
 pip3 install requests-html
 pyppeteer-install
+
+OLD_SCRIPT_NAME=srelogin
+
+mkdir /usr/local/cluster-login;
+pushd /usr/local/cluster-login;
+
+wget https://raw.githubusercontent.com/mbarnes/work-environment/master/files/bin/${OLD_SCRIPT_NAME};
+chmod +x ${OLD_SCRIPT_NAME};
+mv ${OLD_SCRIPT_NAME} cluster-login;
+ln -s /usr/local/cluster-login/cluster-login /usr/local/bin/cluster-login;
+popd;
